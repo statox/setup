2a455d4b0e942a379395716c2deca6f46572ce50
Adrien Fabre <adrien.fabre@dashlane.com>
2024-05-08 02:11:00 +0200

    Add basic auth to logstash http plugin

--- roles/user/docker-apps/files/elasticsearch/test.sh
+++ roles/user/docker-apps/files/elasticsearch/test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-LOGSTASH_ENDPOINT='http://raccoon.statox.fr:8888'
-DOMAIN_ENDPOINT='https://raccoon.statox.fr:9200'
-ELASTIC_USER='elastic'
-ELASTIC_PASSWORD='CHANGEME'
-
+# DOMAIN_ENDPOINT='https://raccoon.statox.fr:9200'
+# ELASTIC_USER='elastic'
+# ELASTIC_PASSWORD='CHANGEME'
 # curl -k -H 'Content-Type: application/json' --user "$ELASTIC_USER:$ELASTIC_PASSWORD" -XGET "$DOMAIN_ENDPOINT/_cat/health"
 # curl -k -H 'Content-Type: application/json' --user "$ELASTIC_USER:$ELASTIC_PASSWORD" -XPOST "$DOMAIN_ENDPOINT/test/_doc" --data '{"foo": 1}'
 
-curl -k -H 'Content-Type: application/json' -XPOST "$LOGSTASH_ENDPOINT/test/_doc" --data '{"foo": 3}'
+LOGSTASH_ENDPOINT='http://raccoon.statox.fr:8888'
+LOGGER_USER='logger'
+LOGGER_PASSWORD='CHANGEME'
+
+curl -v -k -H 'Content-Type: application/json' --user "$LOGGER_USER:$LOGGER_PASSWORD" -XPOST "$LOGSTASH_ENDPOINT/test/_doc" --data '{"foo": 6}'
