745765162b1bfdab8cce7c5631ca079d50d757a0
Adrien Fabre <adrien.fabre@dashlane.com>
2024-05-14 21:14:23 +0200

    ELK - Expose logstash through nginx to get SSL

--- roles/user/docker-apps/files/elasticsearch/test.sh
+++ roles/user/docker-apps/files/elasticsearch/test.sh
@@ -6,7 +6,7 @@
 # curl -k -H 'Content-Type: application/json' --user "$ELASTIC_USER:$ELASTIC_PASSWORD" -XGET "$DOMAIN_ENDPOINT/_cat/health"
 # curl -k -H 'Content-Type: application/json' --user "$ELASTIC_USER:$ELASTIC_PASSWORD" -XPOST "$DOMAIN_ENDPOINT/test/_doc" --data '{"foo": 1}'
 
-LOGSTASH_ENDPOINT='http://raccoon.statox.fr:8888'
+LOGSTASH_ENDPOINT='https://logs.statox.fr'
 LOGGER_USER='logger'
 LOGGER_PASSWORD='CHANGEME'
 
