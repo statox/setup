0e1f356aaa1c54528a9a2b48c18d262dd97b4919
statox <me@statox.fr>
2025-02-18 22:08:40 +0100

    Update nvim install script after release 0.10.4 breaks binary names

--- roles/user/nvim/files/install_nvim.sh
+++ roles/user/nvim/files/install_nvim.sh
@@ -5,8 +5,8 @@ BIN_PATH="$BIN_DIRECTORY/nvim"
 
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/neovim/neovim/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-ARTIFACT_URL="https://github.com/neovim/neovim/releases/download/$LATEST_VERSION/nvim.appimage"
-CHECKSUM_URL="https://github.com/neovim/neovim/releases/download/$LATEST_VERSION/nvim.appimage.sha256sum"
+ARTIFACT_URL="https://github.com/neovim/neovim/releases/download/$LATEST_VERSION/nvim-linux-x86_64.appimage"
+CHECKSUM_URL="https://github.com/neovim/neovim/releases/download/$LATEST_VERSION/nvim-linux-x86_64.appimage.sha256sum"
 
 new_checksum_response=$(curl -L $CHECKSUM_URL)
 new_checksum=$(echo $new_checksum_response | cut -d ' ' -f 1)
